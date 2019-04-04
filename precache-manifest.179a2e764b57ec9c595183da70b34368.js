136c7682d60a1c9d5453484a941803c68e2d9537
Joe Berkem <jberkem@gmail.com>
2019-04-04 17:51:28 +0100

    Updates

---
+++ precache-manifest.179a2e764b57ec9c595183da70b34368.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "0913930593d8af903e6edbf0478686bf",
+    "url": "/Team-Select/static/media/logo-purple.09139305.svg"
+  },
+  {
+    "revision": "65ae0cc06efec89e8048",
+    "url": "/Team-Select/static/css/main.24073600.chunk.css"
+  },
+  {
+    "revision": "9fd07a74791d8c26708b",
+    "url": "/Team-Select/static/js/runtime~main.113ff519.js"
+  },
+  {
+    "revision": "29e97e3a234487803a1ff5818dd9a6e8",
+    "url": "/Team-Select/static/media/bg-image.29e97e3a.jpg"
+  },
+  {
+    "revision": "dc3406fc767ab98fe109",
+    "url": "/Team-Select/static/js/2.3789d0e8.chunk.js"
+  },
+  {
+    "revision": "891b5c2a443b8bdf4d01870ac5c51d98",
+    "url": "/Team-Select/static/media/star-icon.891b5c2a.svg"
+  },
+  {
+    "revision": "6fc6fc3292042469555b07113a2fdda7",
+    "url": "/Team-Select/static/media/logo.6fc6fc32.svg"
+  },
+  {
+    "revision": "1af39bef484683c11ff426f05f0ab02e",
+    "url": "/Team-Select/static/media/list-icon.1af39bef.svg"
+  },
+  {
+    "revision": "9480e3f54cfd044f045936a0ff48503f",
+    "url": "/Team-Select/static/media/customise-icon.9480e3f5.svg"
+  },
+  {
+    "revision": "c6ab770f15115e58aa13b450d7eecc07",
+    "url": "/Team-Select/static/media/download-icon.c6ab770f.svg"
+  },
+  {
+    "revision": "65ae0cc06efec89e8048",
+    "url": "/Team-Select/static/js/main.0e4d4060.chunk.js"
+  },
+  {
+    "revision": "4ce12a7ddc71c115b86cc4471e6100bf",
+    "url": "/Team-Select/static/media/noStar-icon.4ce12a7d.svg"
+  },
+  {
+    "revision": "5279bcefb96050c548a2a287fbcaab07",
+    "url": "/Team-Select/static/media/striped-kit.5279bcef.svg"
+  },
+  {
+    "revision": "3c7408c37a004aa297706863dc960616",
+    "url": "/Team-Select/static/media/full-kit.3c7408c3.svg"
+  },
+  {
+    "revision": "52a850a42eaa9dd48da283e41d4d47eb",
+    "url": "/Team-Select/static/media/half-kit.52a850a4.svg"
+  },
+  {
+    "revision": "6d93ab94b1934141b38cb1f07e80d797",
+    "url": "/Team-Select/static/media/Futura Bold.6d93ab94.otf"
+  },
+  {
+    "revision": "cb64a6dbaf68f689e3d3b8eecfc5f9e4",
+    "url": "/Team-Select/static/media/football-pitch-half1.cb64a6db.jpg"
+  },
+  {
+    "revision": "baefdb8318d7b5bc08cd0ed6128a00f7",
+    "url": "/Team-Select/static/media/footy-pitch-half-2.baefdb83.jpg"
+  },
+  {
+    "revision": "238c32cd050af8099eeaa8f50dd04ec9",
+    "url": "/Team-Select/static/media/Ubuntu-Regular.238c32cd.ttf"
+  },
+  {
+    "revision": "dc3406fc767ab98fe109",
+    "url": "/Team-Select/static/css/2.457e6996.chunk.css"
+  },
+  {
+    "revision": "17a0c981a6bc78c12b1d54c5f33af5cb",
+    "url": "/Team-Select/index.html"
+  }
+];
